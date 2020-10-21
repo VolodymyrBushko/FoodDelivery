284a5e51b7e11148fbf688e5eff791ca5f9a440c
Volodymyr <volodymyr.bushko@gmail.com>
2020-10-21 20:07:00 +0300

    add schema for item

---
+++ server/models/Item.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 20
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectID,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 36
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  weight: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Item', schema);
