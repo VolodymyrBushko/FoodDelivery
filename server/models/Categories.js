ffd9bdd79172d0731f2321b66eefe83d3226fd18
Ruslan <smit30@bigmir.net>
2020-10-21 19:26:54 +0300

    add connect to db,and add categories shema

---
+++ server/models/Categories.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 4
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Category', schema);
