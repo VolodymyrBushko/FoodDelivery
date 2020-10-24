fc02b37c5516f8f5a3baf87ef0893c9e01754c87
Volodymyr <volodymyr.bushko@gmail.com>
2020-10-24 19:53:22 +0300

    init routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {ContactsPageComponent} from './pages/contacts-page/contacts-page.component';
+import {AboutPageComponent} from './pages/about-page/about-page.component';
+import {BasketPageComponent} from './pages/basket-page/basket-page.component';
+import {ProfilePageComponent} from './pages/profile-page/profile-page.component';
+import {CategoryPageComponent} from './pages/category-page/category-page.component';
+import {OrderPageComponent} from './pages/order-page/order-page.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {RegisterPageComponent} from './pages/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'contacts', component: ContactsPageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'basket', component: BasketPageComponent},
+  {path: 'profile', component: ProfilePageComponent},
+  {path: 'category/:id', component: CategoryPageComponent},
+  {path: 'order', component: OrderPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
