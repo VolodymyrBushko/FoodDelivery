1f457e5926ead05e40ac9c0ed015db32cbc0241a
Ruslan <smit30@bigmir.net>
2020-10-26 22:17:21 +0200

    add component ontroll-radio-group

--- src/app/components/controll-radio-group/controll-radio-group.component.ts
+++ src/app/components/controll-radio-group/controll-radio-group.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./controll-radio-group.component.css']
 })
 export class ControllRadioGroupComponent implements OnInit {
-
+  imageUrl_1="../../../assets/images/controll-radio-group/items.svg";
+  imageUrl_2="../../../assets/images/controll-radio-group/categories.svg";
+  imageUrl_3="../../../assets/images/controll-radio-group/order.svg";
+  imageUrl_4="../../../assets/images/controll-radio-group/users.svg";
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  itemsClick(){}
+  componentsClick(){}
+  ordersClick(){}
+  usersClick(){}
 }
