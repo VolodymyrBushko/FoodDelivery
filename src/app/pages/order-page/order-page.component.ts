b79edcec8805967ed0efc47220904896f700c06c
Volodymyr <volodymyr.bushko@gmail.com>
2020-10-28 17:40:58 +0200

    add order page template and order validator

--- src/app/pages/order-page/order-page.component.ts
+++ src/app/pages/order-page/order-page.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
+
+import {AuthService} from '../../services/auth.service';
+import validators from '../../validators/order.validator';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
   styleUrls: ['./order-page.component.css']
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnDestroy {
 
-  constructor() { }
+  form: FormGroup;
+  sub: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.form = formBuilder.group({
+      'address': [null, validators.address],
+      'phone': [null, validators.phone],
+      'email': [null, validators.email]
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
 }
