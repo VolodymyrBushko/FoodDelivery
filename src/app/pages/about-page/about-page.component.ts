788269829150014f6907b5600ba49c2c099977d8
Andriy <aolijnyk73@gmail.com>
2020-10-29 20:00:49 +0200

    update about page

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPageComponent implements OnInit {
 
+  carts=[
+      {path:"../../../assets/images/bigerHome.jpg", text:"some text about goods 11111"},
+      {path:"../../../assets/images/pic2.jpg", text:"some text about goods 22222"},
+      {path:"../../../assets/images/pic1.jpg", text:"some text about goods 333333"}]
+  cartItems = [];
+  totalPrice: number=0;
   constructor() { }
 
   ngOnInit(): void {
+    this.cartItems=JSON.parse(localStorage.getItem('cart'));
   }
 
 }
